e46f58c43ac6580063c8c2dccb7242fd9838c1fb
AngelRots <meowsrail@gmail.com>
2025-01-04 21:14:45 +0100

    Update angel_init.lua

--- lua/autorun/angel_init.lua
+++ lua/autorun/angel_init.lua
@@ -1,16 +1,25 @@
 module( "angelcore", package.seeall ) 
 local AGCore = AGCore or {}
 
+AGCore.Version = "1.0.0"
+AGCore.Prefix = "[AGCore] - "
+
+AGCore.LOG = {
+    INFO = "INFO",
+    WARNING = "WARNING",
+    ERROR = "ERROR"
+}
+
 -- Wrapper for LUA includes 
 -- @param file: Path to the LUA file that needs to be included
 function AGCore:IncludeLUA( file )
 
     if file then 
-        ADDCSLuaFile( file )
+        AddCSLuaFile( file )
         include( file )
-        print("[AGCore] - One or several files were included through the wrapper!")
+        print(AGCore.Prefix .. "One or several files were included through the wrapper!")
     else 
-        print( "[AGCore] - Core include is nil or invalid!" )
+        print( AGCore.Prefix .. "Core include is nil or invalid!" )
 
     end
 end
@@ -22,5 +31,24 @@ function AGCore:IncludeTable( files )
  for _, file in pairs(files) do 
     self:IncludeLUA(file)
     end
+
+end
+
+-- Logging function  
+-- @param logEvent: The string to print to the console
+-- @param logType: The type of log to print ex. INFO, WARNING, ERROR
+-- @see AGCore.LOG
+function AGCore:Log(logEvent,logType)
+
+    local logType = logType or "INFO"
+
+    if not AGCore.LOG[logType] then 
+        logType = "INFO"
+    end
+
+
+    local logMsg = string.format("[%s] - %s", logType, logEvent)
+
+    print(logMsg)
 
 end
