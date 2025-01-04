5978b23821e1b5bd0797b933914b1136de4eae3c
AngelRots <meowsrail@gmail.com>
2025-01-04 20:51:53 +0100

    Update angel_init.lua

--- lua/autorun/angel_init.lua
+++ lua/autorun/angel_init.lua
@@ -2,6 +2,8 @@
 module( "angelcore", package.seeall ) 
 local AGCore = AGCore or {}
 
+-- Wrapper for LUA includes 
+-- @param file: Path to the LUA file that needs to be included
 function AGCore:IncludeLUA( file )
 
     if file then 
@@ -12,4 +14,14 @@ function AGCore:IncludeLUA( file )
         print( "[AGCore] - Core include is nil or invalid!" )
 
     end
+end
+
+-- Wrapper for including LUA files from tables
+-- @param files: Table containing the paths of the LUA files
+function AGCore:IncludeTable( files )
+
+ for _, file in pairs(files) do 
+    self:IncludeLUA(file)
+    end
+
 end
